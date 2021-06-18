9b54536db00b17f9cb4e0594f04b405fe357fa1e
Nikhilesh-Bhatt <85735215+Nikhilesh-Bhatt@users.noreply.github.com>
2021-06-18 13:47:56 +0530

    Add files via upload

---
+++ MCA 2-A/NikhileshBhatt-2001094/Qs2/Qs2.java
@@ -0,0 +1,13 @@
+class Sum
+{
+	public static void main(String args[])
+{
+	int n1,n2,n3,result;
+	
+	n1=Integer.parseInt(args[0]);
+	n2=Integer.parseInt(args[1]);
+	n3=Integer.parseInt(args[2]);
+	result=n1+n2+n3;
+	System.out.println("The sum of three numbers is "+result);
+	}
+}
